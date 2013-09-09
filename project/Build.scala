3a7b534bec994b7a52215a1460607bcffbd5069e
William Billingsley <wbillingsley@cantab.net>
2013-09-09 10:57:43 +1000

    Release as 0.1

--- project/Build.scala
+++ project/Build.scala
@@ -6,12 +6,12 @@ object ApplicationBuild extends Build {
   
     val orgName         = "com.wbillingsley"
     val appName         = "handy-play-oauth"
-    val appVersion      = "0.1-SNAPSHOT"
+    val appVersion      = "0.1"
      
     val appDependencies = Seq(
-      "com.wbillingsley" %% "handy" % "0.4-SNAPSHOT",
-      "com.wbillingsley" %% "handy-play" % "0.4-SNAPSHOT",
-      "com.wbillingsley" %% "salt-encrypt" % "0.1-SNAPSHOT"      
+      "com.wbillingsley" %% "handy" % "0.4",
+      "com.wbillingsley" %% "handy-play" % "0.4",
+      "com.wbillingsley" %% "salt-encrypt" % "0.1"      
     )
 
   lazy val aaaMain = play.Project(appName, appVersion, appDependencies).settings(
